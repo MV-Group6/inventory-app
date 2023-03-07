5e241bb3bb07b7597edb3c11039fddb06d68f4a5
A-Rifkan <a.rifkan2019@gmail.com>
2023-03-07 16:34:56 +0000

    Made modules, Express route and GET for items from table

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,27 +1,2 @@
-const express = require("express");
-const router = express.Router();
-const { Item } = require("../models/item");
-
-// GET
-
-//Express Route to GET all Items
-router.get("/items", async (req, res, next) => {
-  try {
-    const items = await Item.findAll();
-    res.send(items);
-  } catch (error) {
-    next(error);
-  }
-});
-
-//Express Route to GET one Item
-router.get("/items/:id", async (req, res, next) => {
-    try {
-      const item = await Item.findByPk(req.params.id);
-      res.send(item);
-    } catch (error) {
-      next(error);
-    }
-  });
 
 module.exports = router;
