cc4ef32f988cbce2c574455b203a2ec11a0c67a4
Vrajolas <113854426+Vrajolas@users.noreply.github.com>
2023-03-08 11:32:34 +0000

    made frontend for each category and connected webpages

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -3,6 +3,14 @@ import {ItemsList } from './ItemsList';
 import NavBar from './NavBar';
 import '/style.css'
 
+//Importing pages
+import {Home} from './Home';
+import {Mens} from './Mens';
+import {Womens} from './Womens';
+import {Jewel} from './Jewel';
+import {Electronics} from './Electronics';
+import { SingleImg } from './single';
+
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
 
@@ -25,10 +33,33 @@ export const App = () => {
 		fetchItems();
 	}, []);
 
+	//Front end dev
+	let component
+	switch (window.location.pathname) {
+		case "/Home":
+			component = <Home />
+			break
+		case "/Mens":
+			component = <Mens items={items} />
+			break
+		case "/Womens":
+			component = <Womens items={items}/>
+			break
+		case "/Jewel":
+			component = <Jewel items={items}/>
+			break
+		case "/Electronics":
+			component = <Electronics items={items}/>
+			break
+		case "/single":
+			component = <SingleImg items={items}/>
+	}
+
 	return (
-		<main>	
-			<NavBar />
-		</main>
+		<>
+			<NavBar/>
+			{component}
+		</>
 	)
 }
 
--- public/react/components/NavBar.js
+++ public/react/components/NavBar.js
@@ -6,10 +6,11 @@ const NavBar = () => {
         <nav>
             <label class='logo'>Shop With Us</label>
             <ul>
-                <li><a href="#">Men's Clothing</a></li>
-                <li><a href="#">Women's Clothing</a></li>
-                <li><a href="#">Jewelry</a></li>
-                <li><a href="#">Electronics</a></li>
+                <li><a href="./Home">Home</a></li>
+                <li><a href="./Mens">Men's Clothing</a></li>
+                <li><a href="./Womens">Women's Clothing</a></li>
+                <li><a href="./Jewel">Jewelry</a></li>
+                <li><a href="./Electronics">Electronics</a></li>
             </ul>
         </nav>
     );
